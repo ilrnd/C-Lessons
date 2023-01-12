bcbe9b9ba21f34fd56748213ff210c910f036f09
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-12 19:01:50 +0300

    add Example 005 If Else

---
+++ Example005_Condition IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "илья")
+{
+  Console.WriteLine("Ура, это же Илья");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
